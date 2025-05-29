ecc05b27e723d4f0915424d331be65893f00bb4e
milaiablks <camila.iablonski.santos@escola.pr.gov.br>
2025-05-29 20:25:02 -0300

    adiciona array de curiosidades

---
+++ script.js
@@ -0,0 +1,6 @@
+const curiosidades = [
+  "Você sabia que mais de 70% dos alimentos nas cidades vêm de pequenas propriedades rurais?",
+  "Feiras livres são uma ponte direta entre produtores e consumidores urbanos.",
+  "O turismo rural tem crescido muito e ajuda a economia do campo.",
+  "Aplicativos já conectam diretamente agricultores e moradores das cidades."
+];
